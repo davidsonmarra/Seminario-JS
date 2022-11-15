3431edfcc7891e757efaed8bd4398745637deb70
Davidson Marra <davidson.vieira.ext@vr.com.br>
2022-11-14 22:23:45 -0300

    first commit

---
+++ exemplo.java
@@ -0,0 +1,9 @@
+public class Retangulo {
+  private int altura;
+  private int largura;
+
+  public Retangulo(int altura, int largura) {
+    this.altura = altura;
+    this.largura = largura;
+  } // Contrutor
+}
---
+++ exemplo.js
@@ -0,0 +1,2 @@
+let numero = 10;
+numero = '10';
