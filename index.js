5b97b5fb903af6f57b33d4e6af7b1ed69fb28a46
Davidson Marra <davidson.vieira.ext@vr.com.br>
2022-11-17 16:56:37 -0300

    final fix

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 new fullpage('#fullPage', {
   autoScrolling: true,
   navigation: true,
-  navigationTooltips: ['JavaScript', 'QR Code', 'Sumário', 'Introdução', 'Contexto', 'Criador', 'Normalização', 'Histórico', 'Paradigma', 'Imperativo', 'Procedural x OO', 'Funcional', 'JS x Haskell', 'Características 1','Características 2', 'Características 3', 'Java', 'TypeScript', 'Scheme', 'Prolog', 'Frameworks', 'React JS', 'React Native', 'Node.js', 'Cases', 'Instalação', 'Exemplos', 'Concluão', 'Concluão 2', 'Referências', 'Agradecimentos', 'Referências Secundárias'],
+  navigationTooltips: ['JavaScript', 'QR Code', 'Sumário', 'Introdução', 'Contexto', 'Criador', 'Normalização', 'Histórico', 'Paradigma', 'Imperativo', 'Procedural x OO', 'Funcional', 'JS x Haskell', 'Características 1','Características 2', 'Características 3', 'Java', 'TypeScript', 'Scheme', 'Prolog', 'Frameworks', 'React JS', 'React Native', 'Node.js', 'Cases', 'Instalação', 'Exemplos', 'Conclusão', 'Conclusão 2', 'Referências', 'Agradecimentos', 'Referências Secundárias'],
 });
